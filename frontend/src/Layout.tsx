9824cf3a448e56d19a72f827378eb5cb3a7edd61
jainex17 <jainexp017@gmail.com>
2025-06-05 12:51:03 +0530

    analytics

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -3,6 +3,7 @@ import { AppSidebar } from "@/components/sidebar/app-sidebar";
 import { AuthStoreProvider } from "./store/AuthStore";
 import { PortfolioStoreProvider } from "./store/PortfolioStore";
 import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -17,11 +18,12 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             color: "#ffffff",
             border: "1px solid #3A3A3A",
           },
-        }}   />
+        }} />
         <div className="flex-1 overflow-auto bg-background rounded-xl shadow-sm border">
           {children}
         </div>
       </main>
+      <Analytics />
     </SidebarProvider>
     </PortfolioStoreProvider>
     </AuthStoreProvider>
